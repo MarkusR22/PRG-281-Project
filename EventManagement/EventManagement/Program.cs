4c1573e64b3b1edcdb673c8de3c0e8f2ebebe914
MarkusR22 <markusreblin22@gmail.com>
2024-08-15 10:10:11 +0200

    asdf

--- EventManagement/EventManagement/Program.cs
+++ EventManagement/EventManagement/Program.cs
@@ -13,6 +13,7 @@ namespace EventManagement
             //Caydan Frank
             //Hello people
             //This is a comment
+            //markus
             Console.WriteLine("Hello Gooner!");
         }
     }
