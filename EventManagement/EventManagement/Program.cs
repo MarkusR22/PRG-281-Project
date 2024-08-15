d404086a09b497a869f8fc749e82ed5e1bee521f
Joseph-Pereira <135823952+Joseph-Pereira@users.noreply.github.com>
2024-08-15 10:59:42 +0200

    tried to commit something

--- EventManagement/EventManagement/Program.cs
+++ EventManagement/EventManagement/Program.cs
@@ -19,6 +19,7 @@ namespace EventManagement
             //gooner alert
             //gooner alert2
             //Efdsjafea //Trying to comment something joseph!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            // adding more shit to see if this shit will work for once 
         }
     }
 }
