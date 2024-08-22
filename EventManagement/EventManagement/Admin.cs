7b90a690e820a58c158d438fca900e0133dea48e
MarkusR22 <markusreblin22@gmail.com>
2024-08-22 10:33:24 +0200

    Added create Event method

--- EventManagement/EventManagement/Admin.cs
+++ EventManagement/EventManagement/Admin.cs
@@ -10,6 +10,8 @@ namespace EventManagement
 
     public class Admin : User
     {
+        private static string connectionString = "Data Source=DESKTOP-TDBJOM7;Initial Catalog=EventManagement;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;"; //Markus' connection string
+
         public Admin(int id, string userName, string password) : base(id, userName, password)
         {
 
@@ -59,7 +61,7 @@ namespace EventManagement
 
                         break;
                     case AdminMenuOptions.Create_New_Event:
-
+                        CreateEvent();
 
                         break;
                     case AdminMenuOptions.Register_New_Organizer:
@@ -85,6 +87,70 @@ namespace EventManagement
 
         }
 
+        public void CreateEvent()
+        {
+            Console.Write("Enter name of event: ");
+            string eventName = Console.ReadLine();
+            Console.Write("Enter description of event: ");
+            string eventDescription = Console.ReadLine();
+            Console.Write("Enter date of event: ");
+            DateTime eventDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Enter location of event: ");
+            string eventLocation = Console.ReadLine();
+            Console.Write("Enter event organizer ID: ");
+            int organizerID = int.Parse(Console.ReadLine());
+            Console.Write("Enter status of event: ");
+            string eventStatus = Console.ReadLine();
+            Console.Write("Enter ticket price: ");
+            double ticketPrice = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand("INSERT INTO dbo.event(name, description, date, location, organizerID, status, ticket_price) VALUES (@name, @description, @date, @location, @organizerID, @status, @ticket_price)", connection);
+                    command.Parameters.AddWithValue("@name", eventName);
+                    command.Parameters.AddWithValue("@description", eventDescription);
+                    command.Parameters.AddWithValue("@date", eventDate);
+                    command.Parameters.AddWithValue("@location", eventLocation);
+                    command.Parameters.AddWithValue("@organizerID", organizerID);
+                    command.Parameters.AddWithValue("@status", eventStatus);
+                    command.Parameters.AddWithValue("@ticket_price", ticketPrice);
+
+
+
+                    int rowsAffected = (int)command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine("Event created successfully! Press any key to return to menu");
+                        Console.ReadKey();
+                        DisplayMenu();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to create event. Press any key to go back...");
+                        Console.ReadKey();
+                        DisplayMenu();
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                DisplayMenu();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                Console.ReadKey();
+                DisplayMenu();
+            }
+        }
 
         public override void Logout()
         {
