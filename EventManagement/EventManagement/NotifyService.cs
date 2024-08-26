fc12d07db6f41652fd8da643856fdc48232edd71
MarkusR22 <markusreblin22@gmail.com>
2024-08-26 17:28:02 +0200

    Added event for registering for event

--- EventManagement/EventManagement/NotifyService.cs
+++ EventManagement/EventManagement/NotifyService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 namespace EventManagement
 {
@@ -13,10 +14,12 @@ namespace EventManagement
         {
             Admin.EventApproved += OnEventApproved;
             Admin.EventCancelled += OnEventCancelled;
+            Participant.RegisteredForEvent += OnRegisteredForEvent; 
         }
 
         public void OnEventApproved(object sender, EventArgs e)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("Notifying organizer");
             Thread.Sleep(1000);
             Console.Write(".");
@@ -26,10 +29,12 @@ namespace EventManagement
             Console.Write(".");
             Thread.Sleep(1000);
             Console.WriteLine("\nOrganizer has been notified that his event was approved.");
+            Console.ResetColor();
         }
 
         public void OnEventCancelled(object sender, EventArgs e)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.Write("Notifying organizer");
             Thread.Sleep(1000);
             Console.Write(".");
@@ -48,6 +53,15 @@ namespace EventManagement
             Console.Write(".");
             Thread.Sleep(1000);
             Console.WriteLine("\nAll attendees have been notified that the event was cancelled.");
+            Console.ResetColor();
+        }
+
+        public void OnRegisteredForEvent(object sender, RegisteredForEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("successfully registered for event!");
+            Console.WriteLine($"Your entry code is: {e.entryCode}");
+            Console.ResetColor();
         }
 
     }
